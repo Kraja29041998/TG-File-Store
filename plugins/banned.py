fc1c1e85deb625f6c0661dfb7d988c0b754d51db
DKOWNER <94420774+DKOWNER@users.noreply.github.com>
2022-01-05 11:23:36 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -15,4 +15,4 @@ banned_user = filters.create(banned_users)
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
-    await message.reply(f'Sorry Bro, You Are Banned To Use Me. \nBan Reason : {ban["ban_reason"]} \N Ban Duration : {ban["ban_duration"]} \n Ban Time : {ban["banned_on"]}\n\n If You Think This is Mistake. Contact To :- @DKBOTZ')
+    await message.reply(f'Sorry Bro, You Are Banned To Use Me. \nBan Reason : {ban["ban_reason"]} \n Ban Duration : {ban["ban_duration"]} \n Ban Time : {ban["banned_on"]}\n\n If You Think This is Mistake. Contact To :- @DKBOTZ')
