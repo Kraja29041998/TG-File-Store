67bd227168d5c2050f3b98cdb93e61efe464ed74
DKOWNER <94420774+DKOWNER@users.noreply.github.com>
2022-01-05 10:21:53 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,38 @@
+import logging
+from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
+import asyncio
+from pyrogram.types import Message
+from typing import Union
+import re
+import os
+from datetime import datetime
+from typing import List
+from pyrogram.types import InlineKeyboardButton
+from plugins.database.access_db import db
+import requests
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+BTN_URL_REGEX = re.compile(
+    r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))"
+)
+
+
+
+BANNED = {}
+SMART_OPEN = '“'
+SMART_CLOSE = '”'
+START_CHAR = ('\'', '"', SMART_OPEN)
+
+# temp db for banned 
+class temp(object):
+    BANNED_USERS = []
+    BANNED_CHATS = []
+    ME = None
+    CURRENT=int(os.environ.get("SKIP", 2))
+    CANCEL = False
+    MELCOW = {}
+    U_NAME = None
+    B_NAME = None
+
