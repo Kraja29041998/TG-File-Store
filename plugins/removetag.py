15b28d50638782cd8fdaf7b05e040ba172f75ac4
DKOWNER <94420774+DKOWNER@users.noreply.github.com>
2022-01-06 07:17:35 +0530

    Create removetag.py

---
+++ plugins/removetag.py
@@ -0,0 +1,17 @@
+import pyrogram
+import os
+from pyrogram import Client as dkbotz, filters
+from pyrogram.types import Message, User
+
+
+
+
+@dkbotz.on_message(filters.forwarded & filters.channel)
+async def channeltag(bot, message):
+   try:
+       chat_id = message.chat.id
+       forward_msg = await message.copy(chat_id)
+       await message.delete()
+   except:
+       await message.reply_text("Oops , Recheck My Admin Permissions & Try Again")
+    
