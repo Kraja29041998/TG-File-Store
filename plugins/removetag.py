3730458790665c9e992210f244e6247ed4ef6b33
DKOWNER <94420774+DKOWNER@users.noreply.github.com>
2022-01-06 07:26:36 +0530

    Update removetag.py

--- plugins/removetag.py
+++ plugins/removetag.py
@@ -3,15 +3,16 @@ import os
 from pyrogram import Client as dkbotz, filters
 from pyrogram.types import Message, User
 
+#Working Bro My Bot in But I Add This Bot in Next Time
+#Creadit :- @DKBOTZ And @bughunter0
 
 
-
-@dkbotz.on_message(filters.forwarded & filters.channel)
-async def channeltag(bot, message):
-   try:
-       chat_id = message.chat.id
-       forward_msg = await message.copy(chat_id)
-       await message.delete()
-   except:
-       await message.reply_text("Oops , Recheck My Admin Permissions & Try Again")
+#@dkbotz.on_message(filters.forwarded & filters.channel)
+#async def channeltag(bot, message):
+#   try:
+#       chat_id = message.chat.id
+#       forward_msg = await message.copy(chat_id)
+#       await message.delete()
+#   except:
+#       await message.reply_text("Oops , Recheck My Admin Permissions & Try Again")
     
